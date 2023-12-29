3911d2897709162cb534147bc1294ce8a92cccb2
Leele-lee <1623450919@qq.com>
2023-12-29 15:25:27 +0800

    correct format in 2-83

---
+++ chapter2/2-91.c
@@ -0,0 +1,16 @@
+/* 0x40490fdb
+ * 0  100 0000 0  100 1001 0000 1111 1101 1011
+ * A. the fractional binary number 0b11.0010010000111111011011
+ * B. fractional binary representation of 22/7
+ * 22/ 7 = 3 + 1/7 = 11.001001(001)..
+ * C. 9th
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *//
