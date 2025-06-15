08f1e151b9c6cf9ca8eead480b2ceefd29242be7
Leele-lee <1623450919@qq.com>
2025-06-15 15:57:08 +0800

    done with subf.c of 12-38

---
+++ chapter12/12-38/sbuf.c
@@ -0,0 +1,37 @@
+#include "csapp.h"
+#include "sbuf.h"
+
+/* Create an empty, bounded, shared FIFO buffer with n slots */
+void sbuf_init(sbuf_t *sp, int n) {
+    sp->buf = calloc(n, sizeof(int));
+    sp->n = n;
+    sp->front = sp->rear = 0;
+    Sem_init(&sp->mutex, 0, 1);
+    Sem_init(&sp->slots, 0, n);
+    Sem_init(&sp->items, 0, 0);
+}
+
+/* Clean up buffer sp */
+void sbuf_deinit(sbuf_t *sp) {
+    Free(sp->buf);
+}
+
+/* Insert item onto the rear of shared buffer sp */
+void sbuf_insert(sbuf_t *sp, int item) {
+    P(&sp->slots);         // wait for avaliable slot, use a slot
+    P(&sp->mutex);         // lock the buf
+    sp->buf[(++sp->rear) % (sp->n)] = item;
+    V(&sp->mutex);         // unlock the buf
+    V(&sp->items);         // announce avaliable item, add a item
+}
+
+/* Remove and return the first item from buffer sp */
+int sbuf_remove(sbuf_t *sp) {
+    int item;
+    P(&sp->items);
+    P(&sp->mutex);
+    item = sp->buf[(++sp->front) % (sp->n)];
+    P(&sp->mutex);
+    V(&sp->slots);
+    return item;
+}
